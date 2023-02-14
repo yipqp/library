22d7fc2c1445f3ac6dee8db657dd5fdc7f0cb9d5
yipqp <rdyip892@gmail.com>
2023-02-13 17:31:28 -0800

    Design page layout

---
+++ script.js
@@ -0,0 +1,10 @@
+let library = [];
+
+class Book {
+  constructor(author, title, pages, isRead) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.isRead = isRead;
+  }
+}
