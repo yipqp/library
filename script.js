aa021aa1e139f4f19f9f10de30ee8ce5afca744d
yipqp <rdyip892@gmail.com>
2023-02-13 19:11:38 -0800

    Design add book popup

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-let library = [];
+const formPopup = document.querySelector(".form-popup");
+const addBookButton = document.querySelector(".add-book-button");
+const cancelButton = document.querySelector(".cancel-button");
+const blurContainer = document.querySelector(".blur-container");
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const numPages = document.querySelector("#pageNumber");
+
+function clearInputs() {
+  titleInput.value = "";
+  authorInput.value = "";
+  numPages.value = "";
+}
+
+addBookButton.addEventListener("click", () => {
+  formPopup.style.display = "block";
+  blurContainer.style.display = "block";
+  blurContainer.classList.add("blur");
+});
+
+cancelButton.addEventListener("click", () => {
+  formPopup.style.display = "none";
+  blurContainer.style.display = "none";
+  clearInputs();
+});
+
+const library = [];
 
 class Book {
   constructor(author, title, pages, isRead) {
